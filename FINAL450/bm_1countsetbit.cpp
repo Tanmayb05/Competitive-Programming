5e84673587120e97b4768661540f5786d3c57e65
Tanmay Bhuskute <tanmaybhuskute8@gmail.com>
2021-05-28 11:49:16 +0530

    Create bm_1countsetbit.cpp

---
+++ FINAL450/bm_1countsetbit.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+#include<iostream>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    int setBits(int N) {
+        // Write Your Code here
+        int a[50], i=0, count=0;
+
+        for(i=0; N>0; i++)  {
+            a[i] = N%2;    N = N/2;  
+        } 
+
+        for(i=i-1; i>=0; i--) {
+            if(a[i] == 1)
+                count++;
+        }
+
+    //To show the binary number    
+    /*
+        int j=i-1;
+
+        for(i=i-1 ;i>=0 ;i--)    
+            cout<<a[i];    
+        cout<<endl;
+
+        for(j=i; j>=0; j--) {
+            if(a[j] == 1)
+                count++;
+        }
+    */
+
+        return count;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
